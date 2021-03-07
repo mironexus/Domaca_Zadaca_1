b985c8a1c7656376781caaf27419d95ef56a03c3
Miro <miro.nexus5x@gmail.com>
2021-03-07 13:03:04 +0100

    Added Button and TextView

--- app/src/main/java/com/example/domacazadaca1/MainActivity.kt
+++ app/src/main/java/com/example/domacazadaca1/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.domacazadaca1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.domacazadaca1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toggleButton.setOnClickListener{
+            binding.helloWorld.visibility = if (binding.helloWorld.visibility == View.VISIBLE) { View.INVISIBLE } else { View.VISIBLE }
+            binding.toggleButton.text = if (binding.toggleButton.text == getString(R.string.hide)) { getString(R.string.show) } else { getString(R.string.hide) }
+        }
     }
 }
