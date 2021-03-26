fd83dda7e1290f2c9ec04dd1237a346f7e6c90dd
Miro <miro.nexus5x@gmail.com>
2021-03-26 19:32:57 +0100

    Functional CustomView with xml attributes

--- app/src/main/java/com/example/domacazadaca1/contacts/add/TextInputView.kt
+++ app/src/main/java/com/example/domacazadaca1/contacts/add/TextInputView.kt
@@ -1,7 +1,9 @@
 package com.example.domacazadaca1.contacts.add
 
 import android.content.Context
+import android.text.InputFilter
 import android.text.InputType
+import android.text.InputType.*
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
@@ -17,14 +19,14 @@ class TextInputView(context: Context, attrs: AttributeSet) : FrameLayout(context
 
     private var binding: InputEdittextBinding
 
-    var label_text: String?
-    var hint_text: String?
+//    var label_text: String?
+//    var hint_text: String?
 
     init {
         val layoutInflater =
             context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         binding = InputEdittextBinding.inflate(layoutInflater)
-        inflate(context, R.layout.input_edittext, binding.root)
+        inflate(context, R.layout.input_edittext, this)
         addView(binding.root)
 
         context.theme.obtainStyledAttributes(
@@ -32,23 +34,32 @@ class TextInputView(context: Context, attrs: AttributeSet) : FrameLayout(context
             R.styleable.TextInputView,
             0, 0).apply {
             try {
-                label_text = getString(R.styleable.TextInputView_label)
-                hint_text = getString(R.styleable.TextInputView_input)
+                val label_text = getString(R.styleable.TextInputView_label)
+                val hint_text = getString(R.styleable.TextInputView_hint)
                 val dataType = getInt(R.styleable.TextInputView_dataType, DataType.NUMBER.ordinal)
-                val length = getInt(R.styleable.TextInputView_stringLength, 0)
+                val length = getInt(R.styleable.TextInputView_inputLength, 0)
+
+                binding.label.text = label_text
+                binding.input.hint = hint_text
+
+
                 when (dataType) {
                     DataType.NUMBER.ordinal -> {
-                        binding.input.inputType = InputType.TYPE_CLASS_NUMBER
+                        binding.input.inputType = TYPE_CLASS_NUMBER
+                        when(binding.label.text) {
+                            resources.getString(R.string.label_age)  -> { binding.input.filters += InputFilter.LengthFilter(3) }
+                            resources.getString(R.string.label_oib) -> { binding.input.filters += InputFilter.LengthFilter(13) }
+                        }
+
                     }
                     DataType.STRING.ordinal -> {
-
+                        binding.input.inputType = TYPE_CLASS_TEXT
                     }
                     DataType.MAIL.ordinal -> {
 
                     }
                 }
-                binding.label.text = label_text
-                binding.input.hint = hint_text
+
             }
             finally {
                 recycle()
